80dc41b79391044b22519440d9babd51b9710518
tiffany <ting11222001@gmail.com>
2023-01-27 11:09:01 +1030

    fix: 🐛 udpate navbar text align

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,11 +14,17 @@ const Navbar = () => (
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-betwteen gap-8`}>
-      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        METEAVERSUS
-      </h2>
-      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
+      <div>
+        <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
+      </div>
+      <div className="grow text-center">
+        <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+          METEAVERSUS
+        </h2>
+      </div>
+      <div>
+        <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
+      </div>
     </div>
   </motion.nav>
 );
