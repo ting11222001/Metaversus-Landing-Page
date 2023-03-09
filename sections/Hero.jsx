e6f4874f9594140962452b818fbbcb1315317668
tiffany <ting11222001@gmail.com>
2023-03-09 22:35:46 +1030

    fix: 🐛 remove unused code

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { staggerContainer, textVariant } from '../utils/motion';
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
