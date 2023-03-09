5b1c7b6f4301d2c1f4d9260457fc88fcaa7a3719
tiffany <ting11222001@gmail.com>
2023-03-09 22:53:24 +1030

    fix: 🐛 explore card animation

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -7,8 +7,10 @@ import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
-    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-    className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'} flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    initial={{ opacity: 0, translateX: -50 }}
+    animate={{ opacity: 1, translateX: 0 }}
+    transition={{ duraction: 0.3 }}
+    className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'} flex items-center justify-center min-w-[170px] h-[700px] cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img src={imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
