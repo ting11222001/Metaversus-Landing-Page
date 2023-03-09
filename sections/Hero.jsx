4ea850f31e597582b74fedfb116935f413500354
tiffany <ting11222001@gmail.com>
2023-03-09 22:25:47 +1030

    fix: 🐛 banner, title and subtitle animation in hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -15,11 +15,11 @@ const Hero = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
-        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+        <motion.h1 variants={textVariant(0.1)} className={styles.heroHeading}>
           Metaverse
         </motion.h1>
         <motion.div
-          variants={textVariant(1.2)}
+          variants={textVariant(0.2)}
           className="flex flex-row justify-center items-center"
         >
           <h1 className={styles.heroHeading}>Ma</h1>
@@ -29,7 +29,8 @@ const Hero = () => (
       </div>
 
       <motion.div
-        variants={slideIn('right', 'tween', 0.2, 1)}
+        initial={{ opacity: 0, translateX: -50 }}
+        animate={{ opacity: 1, trnaslateX: 0 }}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
         <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
