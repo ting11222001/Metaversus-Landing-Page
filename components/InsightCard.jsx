ab880c48770942537d13c7a89fe556dbd1bb64ad
tiffany <ting11222001@gmail.com>
2023-03-21 22:41:59 +1030

    fix: 🐛 update image component in the InsightCard

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../utils/motion';
 
@@ -8,7 +9,12 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
-    <img src={imgUrl} alt="planet-01" className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover" />
+    <Image
+      src={imgUrl}
+      alt="planet"
+      width={600}
+      height={450}
+    />
     <div className="w-full flex justify-between items-center">
       <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
         <h4 className="font-normal lg:text-[42px] text-[26px] text-white">{title}</h4>
