d3545acf595ed287dd46f9f8cfc1eed6351b2a6a
tiffany <ting11222001@gmail.com>
2023-03-21 22:59:41 +1030

    fix: 🐛 remove gradient

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,7 +14,6 @@ const Footer = () => (
     whileInView="show"
     className={`${styles.paddings} py-8 relative`}
   >
-    <div className="footer-gradient" />
 
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
